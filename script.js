a885c65f39e47393681802d5395b27a87c413122
yass09 <y.chibane@me.com>
2016-04-13 14:55:56 +0200

    changed url to captcha version

--- script.js
+++ script.js
@@ -23,19 +23,17 @@ function generate() {
     size = document.querySelector("#size-selector").value;
     console.log(model + " " + size);
     result = document.querySelector(".link-result");
-    link = "https://www.adidas.fr/on/demandware.store/Sites-adidas-FR-Site/fr_FR/Cart-MiniAddProduct?pid=%20" + model + "_" + size + "&pid=%20" + model + "_%20" + size;
-//    link = a.href;
+    linkCaptcha = "http://www.adidas.fr/on/demandware.store/Sites-adidas-FR-Site/en_FR/Cart-MiniAddProduct?layer=Add+To+Bag+overlay&pid=%20" + model + "_" + size + "&pid=%20" + model + "_%20" + size + "&g-recaptcha-response=&Quantity=1&masterPid=BA8599&ajax=true";
+    link = a.href;
     result.value = link;
     console.log(link);
 };
 
 function openLink() {
-     window.open(link);
+     window.open(linkCaptcha);
 };
 
 generateButton.addEventListener("click",generate);
 generateButton.addEventListener("click",openLink);
 
-
-
-
+//link = "https://www.adidas.fr/on/demandware.store/Sites-adidas-FR-Site/fr_FR/Cart-MiniAddProduct?pid=%20" + model + "_" + size + "&pid=%20" + model + "_%20" + size;
